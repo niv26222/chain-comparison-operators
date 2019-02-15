9b34fd6051234c1945f31b804fd26bba49bd2f7b
Niv Ben Abat <niv.benavat@gmail.com>
2019-02-15 18:26:16 +0200

    Add files via upload

---
+++ chain_comparison.py
@@ -0,0 +1,23 @@
+
+
+
+# Example to show how to chain comparison operators
+
+
+a, b, c, d , e, f = 0, 5, 12, 0, 15, 15
+
+exp1 = a <= b < c > d is not e is f
+exp2 = a is d > f is not c
+
+
+print(exp1)
+print(exp2)
+
+
+
+
+# ============ OUTPUT ============
+
+# Trur
+# False
+
